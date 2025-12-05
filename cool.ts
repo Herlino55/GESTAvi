90aef5b195609b58f2db55196aeb2fcc9af4e780
Herlin Dongmo <herlin.dongmo@gmail.com>
2025-12-05 04:56:16 +0100

    env

---
+++ cool.ts
@@ -0,0 +1,18 @@
+// NODE_ENV="development"
+// PORT="3000"
+// APP_URL="http://localhost:3000"
+
+// # Database (MySQL/MariaDB)
+// # DATABASE_URL="mysql://user:password@localhost:3306/genuka_express"
+// DATABASE_URL="mysql://root@localhost:3306/gestavi"
+// DB_HOST="localhost"
+// DB_PORT="3306"
+// DB_USER="root"
+// DB_PASSWORD=""
+// DB_NAME="gestavi"
+
+// # Genuka OAuth
+// GENUKA_URL="https://api.genuka.com"
+// GENUKA_CLIENT_ID="019aeb58-a0a3-7197-8922-24ccf2f360ff"
+// GENUKA_CLIENT_SECRET="I2Qa4EUFMkiru39oU9GTCebFjaP7CholxgwUfhDf"
+// GENUKA_REDIRECT_URI="http://localhost:3000/api/auth/callback"
