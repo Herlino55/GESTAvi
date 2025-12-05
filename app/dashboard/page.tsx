428509608f5160f12ff4260e7013aeb920ab9fdf
Mhbbayihe <mhbbayihe12@gmail.com>
2025-12-05 10:31:36 +0100

    batiment

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,19 +1,29 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "@/components/layouts/navbar";
 import Sidebar from "@/components/layouts/sidebar";
 import Index from "@/components/layouts/index";
 
-
 export default function DashboardPage() {
+
+  // 👉 Tu avais oublié ceci !
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
   return (
     <div className="flex h-screen bg-gray-50">
-        <Sidebar />
-        <div className="flex-1 flex flex-col overflow-hidden">
-          <Navbar />
-          <Index />
-        </div>
+
+      {/* Sidebar */}
+      <Sidebar 
+        isOpen={isSidebarOpen}
+        onClose={() => setIsSidebarOpen(false)}
+      />
+
+      {/* Main content */}
+      <div className="flex-1 flex flex-col overflow-hidden">
+        <Navbar onMenuClick={() => setIsSidebarOpen(true)} />
+        <Index />
+      </div>
     </div>
   );
 }
