dd7a8b2691d8f50f967598c80f62b69841e8192d
terravidhal <infiniterraetincelle@gmail.com>
2025-12-05 04:06:21 +0100

    fix unknown Component is an async Client Component

--- dal/get-produt.ts
+++ dal/get-produt.ts
@@ -1,26 +1,24 @@
 export const getProduct = async (company: any) => {
-    console.log("Getting product for company:", company);
-//   const options = {
-//   method: 'GET',
-//   headers: {Authorization: `Bearer ${company.accesstoken}`, 'Content-Type': 'application/json'},
-//   body: JSON.stringify({
-//     content: 'body_html',
-//     handle: 'handle-herzcde',
-//     metadata: {shopifyId: 'id'},
-//     title: 'Title'
-//   })
-// };
+  console.log("Getting product for company:", company);
 
+  const options = {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${company.accessToken}`,
+      "Content-Type": "application/json",
+      "X-Company": company.id,
+    },
+  };
 
-
-const options = {method: 'GET', 
-  headers: {Authorization: `Bearer ${company.accessToken}`, 'Content-Type': 'application/json',"X-Company": company.id},
+  try {
+    const res = await fetch(
+      "https://api-staging.genuka.com/2023-11/admin/products/",
+      options
+    );
+    const data = await res.json();
+    console.log(data);
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
 };
-
-
-
-fetch('https://api-staging.genuka.com/2023-11/admin/products/', options)
-  .then(res => res.json())
-  .then(res => console.log(res))
-  .catch(err => console.error(err));
-}
